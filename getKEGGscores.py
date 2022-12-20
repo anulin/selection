0ffa9f6d7b3f6b523fab78bc778200cf1df2b9ec
anulin <anulin@mail.ru>
2022-12-20 18:39:08 +0800

    Add files via upload

---
+++ getKEGGscores.py
@@ -0,0 +1,135 @@
+import glob
+import os
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+import random as ra
+import matplotlib as mpl
+
+def union(a,b):
+    for i in b:
+        a+=b[i]
+
+x={}
+y={}
+z={}
+"""z - использовалось в старой программе, х-метрики снипов, y - названия генов у белков"""
+"считает метрики сети как среднюю/медианную метрику ее снипов"
+
+metrics={}#словарь списков метрик  для  гена/ dictionary of snp metrics lists for genes
+k=0
+#код от старой программы
+# with open("results.txt") as file:
+#     for line in file:
+#             z[line.split()[1]]=line.split()[0]
+used=set()
+c=2
+print('''getKEGGscores scoresfile --a annotationfile --n networksfile'
+--a (optional) --n (optional)
+--h - help''')
+exit()
+if '--h' in sys.argv:
+    print('''getKEGGscores scoresfile --a annotationfile --n networksfile
+    scoresfile is produced by scores.dll 
+    --a (optional) - custom file with snp to gene annotation
+    --n (optional) - custom file with gene to pathway annotation
+    --p (optional) - percentile. Default is 15%
+    --h - help''')
+    exit()
+if '--a' in sys.argv and len(sys.argv)>=sys.argv.index('--a')+2:
+    annotfile = sys.argv[sys.argv.index('--a')+1]
+    c+=2
+else:
+    annotfile = "annotated_ExomeCeu.txt"
+if '--n' in sys.argv and len(sys.argv)>=sys.argv.index('--n')+2:
+    networkfile = sys.argv[sys.argv.index('--n')+1]
+    c += 2
+else:
+    networkfile="KEGG to Genes Upd.txt"
+
+if len(sys.argv)!=c or sys.argv[1]=='--a' or sys.argv[1]=='--n':
+    raise Exception('Wrong arguments. Use --h to see how to run')#frqfile,  annot file, networkfile, flags
+frqFile=sys.argv[1]
+
+
+with open(frqFile) as file:#открываем файл с метриками и снипами и записываем в словарь х #scoresExomeG-T_maf #scoresvcfExomeCeuMaf
+    for line in file:
+        x[line.split()[0]]=line.split()[1]
+# with open("scoresvcfExomeCeuMaf.txt") as file:
+#     for line in file:
+#         x[line.split()[0]+'_'+line.split()[2]]=line.split()[7]
+
+#annotated_exomeCeuS annotated_ExomeS
+genes=set()
+with open("annotated_ExomeCeu.txt") as file:# открываем файл аннотации нужных сипов и генов и записываем их метрики, используя словарь х
+    for l in file:
+        if l.split()[1] in used or len(l.split())<3:#избегаем повторения снипов
+            print(l)
+            continue
+        if( l.split()[1] in x and (l.split()[2] in genes or l.split()[2] in z.keys() and z[l.split()[2]] in genes)): #если название уже есть в словаре добавляем метрику по ключу в список
+            if (l.split()[2] in z.keys()):# часть старого кода. Проверяет если название хорошее(hgnc)
+                metrics[z[l.split()[2]]].append(float(x[l.split()[1]]))
+            else:
+                metrics[l.split()[2]].append(float(x[l.split()[1]]))
+                z[l.split()[2]] = l.split()[2]
+        elif  l.split()[1] in x : #иначе добавляем название(ключ) в словарь
+            genes.add(l.split()[2])
+            metrics[l.split()[2]]=[float(x[l.split()[1]])]
+            z[l.split()[2]]=l.split()[2]
+        used.add(l.split()[1])
+Goodnames={}
+pvalues={}
+PathwayGenes={}
+ListOfVals=[]
+nnn=set()#genes present in networks
+with open("KEGG to Genes Upd.txt") as file:#KEGG_database  #KEGG to Genes Upd
+    file.readline()
+    for line in file:
+        if line.split()[1] not in PathwayGenes:
+            Goodnames[line.split()[1]]=[line.split()[2]]
+            PathwayGenes[line.split()[1]]=[line.split()[0]]
+        else:
+            PathwayGenes[line.split()[1]].append(line.split()[0])
+        if line.split()[0] in metrics: #if gene is in network
+            nnn.add( line.split()[0])
+for i in nnn:
+    ListOfVals+=metrics[i]
+print(len(x),sum(len(i) for i in metrics))
+print(len(ListOfVals))
+exist='F'
+for pathway in PathwayGenes:#открываем все xml файлы (файлы генных сетей) в папке KEGG_gml; walk through all the kegg names
+    metric = []
+    # metric=0
+    names = set()
+    for gene in PathwayGenes[pathway]:
+        if gene in metrics:
+            metric += metrics[gene]  # cумма листов - лист
+
+            names.add(gene)
+    counter = 0
+    k = 0
+    # print(filename,len(names), len(PathwayGenes[filename[9:-4]]))
+    if (len(metric) == 0):
+        pvalues[pathway] = '1.0 0\t0'
+        continue
+    metric =np.percentile(metric,15)
+    snpCount = sum(len(metrics[gene]) for gene in names)
+
+    if (metric == 0):
+        pvalues[pathway] = '1.0 0 ' + str(snpCount)
+    else:  # если метрика сети не 0 то генерируем случайные сети
+        while (counter < 290 and k < 250000 and not (counter >= 9 and counter / k >= 0.05)):
+            RandMetric = ra.sample(ListOfVals, snpCount)
+            RandMetric = np.percentile(RandMetric, 15)
+            if (metric <= RandMetric):  # если метрика больше то +1 к счетчику таких метрик. Далее counter делится на число генераций k с целью получения p-value
+                counter += 1
+            k += 1
+
+        #print(filename, metric, counter / k, snpCount, len(names))
+
+        pvalues[pathway] = str(counter / k) + '\t' + str((metric)) + '\t' + str(
+            snpCount) + '\t' + str(len(names))  # записываем количество генов метрику и p-value
+with open("pathway_scores.txt",'w') as file:#записываем результат в файл.
+    file.write("Name\tID\tP-value\tMetric\tsnps\tgenes"+'\n')
+    for i in pvalues:# i это название сети и ключ в словаре pvalues. по нему получаем строку с остальными значениями
+        file.write(Goodnames[i]+'\t'+i +'\t'+str(pvalues[i]) +'\n')
---
+++ vcf-vcf.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Globalization;
+
+namespace _1000genomes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double asd = 0;
+            Dictionary<string, int[]> N0 = new Dictionary<string, int[]>();//одна популяция
+            Dictionary<string, int[]> N1 = new Dictionary<string, int[]>();// вторая популяция
+            Dictionary<string, string[]> alleles1 = new Dictionary<string, string[]>();
+            Dictionary<string, string[]> alleles = new Dictionary<string, string[]>();
+            Console.WriteLine(args[0]);
+            int i1, i2;
+            
+            var maf = 0.05;
+            float ind = 1;
+            i1 = int.Parse(args[0]);
+            i2 = int.Parse(args[1]);
+            
+            if (Array.IndexOf(args, "--help") != -1)
+            {
+                Console.Write(@"dotnet scores.dll snpcountsfile1 snpcountsfile2 samplesize1 samplesize2 [--optional flags]
+--help - outputs help
+--maf - minor allele frequency filter (default 0.05)
+--mi - minimal fraction of individuals available for snp (default 1)");
+                return;
+            }
+            
+            int id = Array.IndexOf(args, "--maf");
+            if( id!=-1){
+                maf=float.Parse(args[id + 1]);
+            }
+            id=Array.IndexOf(args, "--mi");
+            if (id != -1)
+            {
+                ind = float.Parse(args[id + 1]);
+            }
+            int muliplier = 2;
+            //readText = File.ReadAllLines("syn.txt");
+            System.Threading.Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+            string[] readText =File.ReadAllLines("data.txt");//читаем файл первой популяции
+            Console.WriteLine(readText[148909]);
+            foreach (string line in readText)
+            {
+
+                string[] s = line.Split('\t');
+                if (s[0][0] == 'Y') muliplier = 1; // Y гаплоидная
+                else muliplier = 2;
+                N0[s[0]] = new int[] { int.Parse(s[1])* 1, int.Parse(s[2]) , int.Parse(s[3]) , int.Parse(s[4]), int.Parse(s[5]) };//записываем частоты снипов первой популяции
+                alleles[s[0]] = new string[] { s[6], s[7], s[8] };//
+                
+            }
+            readText = File.ReadAllLines("dataC.txt");//читаем файл второй популяции
+            foreach(string line in readText)
+            {
+                string[] s = line.Split('\t');
+                if (s[0][0] == 'Y') muliplier = 1; // Y гаплоидная
+                else muliplier = 2;
+                N1[s[0]] = new int[] { int.Parse(s[1])* 1, int.Parse(s[2]), int.Parse(s[3]), int.Parse(s[4]), int.Parse(s[5]) };//записываем частоты снипов второй популяции
+
+            }
+            double n0, n1, p0, p1;//n0, n1 - общие количества образцов первой и второй популяции р0, р1 - соответствующие частоты снипов
+            List<string> text = new List<string>();
+            foreach (string key in N1.Keys)//идем по снипам
+            {
+                if (!N0.ContainsKey(key)) continue;
+                if (N1[key][0] != 0)
+                {
+                    double max = 0;
+                    int j = 0;
+                    n0 = N0[key][0];
+                    n1 = N1[key][0];
+                    if (n0 <= ind * i1 || n1  <= ind * i2) continue;
+                    for (int i = 1; i < 5; i++)//выбираем, которая частота нам больше подходит
+                    {
+                        p0 = N0[key][i] / n0;
+                        p1 = N1[key][i] / n1;
+                        if ((p0 - p1) * (p0 - p1) / (p0 * (1 - p0) / n0 + p1 * (1 - p1) / n1) > max)
+                        {
+                            max = (p0 - p1) * (p0 - p1) / (p0 * (1 - p0) / n0 + p0 * p0 - 2 * p0 * p1 + p1 * (1 - p1) / n1 + p1 * p1 - (p1 - p0) * (p1 - p0)); //(p1 - p0)^2==p0 * p0 - 2 * p0 *...
+                            j = i;
+                        }
+                    }
+
+                    p0 = N0[key][j] / n0;
+                    p1 = N1[key][j] / n1;
+                    if ((p0 <maf && p1 < maf) || ( p0 > 1- maf && p1 > 1- maf)) { continue; }//maf 0.05
+                    //if(p0==p1 && (p0 == 1||p0==0)) { continue; }//отброс нулей
+                    if (p1 - p0 != 0)//вычисляем метрику по формуле и записываем в список результатов имя снипа, метрику, буквы
+                    {
+                        text.Add(key + '\t' + max.ToString() + '\t' + alleles[key][0] + ' ' + alleles[key][1] + ' ' + alleles[key][2]);
+                        if (!Double.IsPositiveInfinity(max))
+                            asd += max;
+                        else Console.Write("sadasdsadasdasdsada");
+                    }
+                    else
+                    {
+                        text.Add(key + '\t' + '0' + '\t' + alleles[key][0] + ' ' + alleles[key][1] + ' ' + alleles[key][2]);
+                    }
+                }
+                else Console.WriteLine("strannoe chislo ludey");
+            }
+            Console.WriteLine(asd);
+
+            File.WriteAllLines($"scoresvcf{ind}.txt", text);//записываем результат в файл
+            using (StreamWriter sw = new StreamWriter("tstscores.txt", false, System.Text.Encoding.Default))
+            {
+                sw.Write(text);
+            }
+            
+            Console.Read();
+        }
+    }
+}
